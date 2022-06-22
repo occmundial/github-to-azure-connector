b921429b1148afba033c30eed6aabac79282220e
Alan García <agarciab@occ.com.mx>
2022-06-22 17:57:36 -0500

    New PAT

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import fetch from 'node-fetch';
-// ("Authorization", "Basic " + btoa('Basic' + ":" + 'YOUR_PAT_TOKEN'));
-// 'Basic v4x2bylpdef6y2ms44yg7l3eb2kc5lfiu5cn2rzpfqqk5swxfzsq'
-// djR4MmJ5bHBkZWY2eTJtczQ0eWc3bDNlYjJrYzVsZml1NWNuMnJ6cGZxcWs1c3d4ZnpzcQ==
+// 'qpftyfvsfdog7y5ar54nsmq4n7mwu5am6gshggbjfvzldy7xztva'
+// 'Basic QmFzaWM6cXBmdHlmdnNmZG9nN3k1YXI1NG5zbXE0bjdtd3U1YW02Z3NoZ2diamZ2emxkeTd4enR2YQ=='
 
-let pat = 'v4x2bylpdef6y2ms44yg7l3eb2kc5lfiu5cn2rzpfqqk5swxfzsq';
+let pat = 'qpftyfvsfdog7y5ar54nsmq4n7mwu5am6gshggbjfvzldy7xztva';
 var server = 'https://dev.azure.com/occmundial/_apis/projects?api-version=2.0';
 var headers = {
     'Content-Type': 'application/json; charset=utf-8;',
-    'Authorization': 'Basic ' + Buffer.from(pat).toString('base64')
+    'Authorization': 'Basic ' + Buffer.from('Basic'+":"+pat).toString('base64')
 };
 const options = {
     method: "GET",
@@ -16,22 +15,12 @@ const options = {
 
 console.log(headers);
 
-//const response = await fetch(server, { method: 'GET', headers: headers})
-
-/*const r1 = response.clone();
-
-const results = await Promise.all([response.json(), r1.text()]);
-
-console.log(results[0]);
-console.log(results[1]); */
-
-
-/* fetch(server, options)
-        //.then(response => response.json())
-        .then(response => {
-            console.log('Web API responds:');
-            console.log(response);
-            //console.log(JSON.stringify(response.value[0], null, 4));
-        }).catch(error => {
-            console.error(error);
-        }); */
+fetch(server, options)
+    //.then(response => response.json())
+    .then(response => {
+        console.log('Web API responds:');
+        console.log(response);
+        //console.log(JSON.stringify(response.value[0], null, 4));
+    }).catch(error => {
+        console.error(error);
+    }); 
