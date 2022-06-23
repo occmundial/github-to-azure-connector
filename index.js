2d645629cae239884704b0d91db1b54652ad641b
Alan García <agarciab@occ.com.mx>
2022-06-23 11:14:47 -0500

    Auth working

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import fetch from 'node-fetch';
-// 'qpftyfvsfdog7y5ar54nsmq4n7mwu5am6gshggbjfvzldy7xztva'
-// 'Basic QmFzaWM6cXBmdHlmdnNmZG9nN3k1YXI1NG5zbXE0bjdtd3U1YW02Z3NoZ2diamZ2emxkeTd4enR2YQ=='
 
 let pat = 'qpftyfvsfdog7y5ar54nsmq4n7mwu5am6gshggbjfvzldy7xztva';
 var server = 'https://dev.azure.com/occmundial/_apis/projects?api-version=2.0';
 var headers = {
     'Content-Type': 'application/json; charset=utf-8;',
-    'Authorization': 'Basic ' + Buffer.from('Basic'+":"+pat).toString('base64')
+    'Authorization': 'Basic ' + Buffer.from(''+":"+pat, 'ascii').toString('base64')
 };
 const options = {
     method: "GET",
@@ -16,11 +14,11 @@ const options = {
 console.log(headers);
 
 fetch(server, options)
-    //.then(response => response.json())
+    .then(response => response.json())
     .then(response => {
         console.log('Web API responds:');
-        console.log(response);
-        //console.log(JSON.stringify(response.value[0], null, 4));
+        //console.log(response);
+        console.log(JSON.stringify(response.value[0], null, 4));
     }).catch(error => {
         console.error(error);
     }); 
