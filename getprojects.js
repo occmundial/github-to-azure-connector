668daa73c0afcc27cab2280bd7ac8919651485e8
Alan García <agarciab@occ.com.mx>
2022-07-13 13:51:53 -0500

    upload node modules

---
+++ getprojects.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+
+
+var server = 'https://dev.azure.com/occmundial/_apis/projects?api-version=2.0';
+var headers = {
+    'Content-Type': 'application/json; charset=utf-8;',
+    'Authorization': 'Basic ' + Buffer.from(''+":"+pat, 'ascii').toString('base64')
+};
+const options = {
+    method: "GET",
+    headers: headers
+};
+
+console.log(headers);
+
+fetch(server, options)
+    .then(response => response.json())
+    .then(response => {
+        console.log('Web API responds:');
+        //console.log(response);
+        console.log(JSON.stringify(response.value[0], null, 4));
+    }).catch(error => {
+        console.error(error);
+    }); 
